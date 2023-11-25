d33abc00642d356a22b631a125a86ecb1c0819ac
Faith Achieng <faith.achieng@student.moringaschool.com>
2023-11-26 00:17:15 +0300

    assignment completed

--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -1,7 +1,6 @@
 import json
-
-from app import app
-from models import db, Plant
+from app import app, db
+from models import Plant
 
 class TestPlant:
     '''Flask application in app.py'''
@@ -9,7 +8,7 @@ class TestPlant:
     def test_plants_get_route(self):
         '''has a resource available at "/plants".'''
         response = app.test_client().get('/plants')
-        assert(response.status_code == 200)
+        assert response.status_code == 200
 
     def test_plants_get_route_returns_list_of_plant_objects(self):
         '''returns JSON representing Plant objects at "/plants".'''
@@ -19,12 +18,12 @@ class TestPlant:
             db.session.commit()
 
             response = app.test_client().get('/plants')
-            data = json.loads(response.data.decode())
-            assert(type(data) == list)
+            data = response.json
+            assert isinstance(data, list)
             for record in data:
-                assert(type(record) == dict)
-                assert(record['id'])
-                assert(record['name'])
+                assert isinstance(record, dict)
+                assert record.get('id')
+                assert record.get('name')
 
             db.session.delete(p)
             db.session.commit()
@@ -33,19 +32,21 @@ class TestPlant:
         '''allows users to create Plant records through the "/plants" POST route.'''
         response = app.test_client().post(
             '/plants',
-            json = {
+            json={
                 "name": "Live Oak",
                 "image": "https://www.nwf.org/-/media/NEW-WEBSITE/Shared-Folder/Wildlife/Plants-and-Fungi/plant_southern-live-oak_600x300.ashx",
                 "price": 250.00,
             }
         )
 
+        assert response.status_code == 201  # 201 status code for successful creation
+
         with app.app_context():
             lo = Plant.query.filter_by(name="Live Oak").first()
-            assert(lo.id)
-            assert(lo.name == "Live Oak")
-            assert(lo.image == "https://www.nwf.org/-/media/NEW-WEBSITE/Shared-Folder/Wildlife/Plants-and-Fungi/plant_southern-live-oak_600x300.ashx")
-            assert(lo.price == 250.00)
+            assert lo
+            assert lo.name == "Live Oak"
+            assert lo.image == "https://www.nwf.org/-/media/NEW-WEBSITE/Shared-Folder/Wildlife/Plants-and-Fungi/plant_southern-live-oak_600x300.ashx"
+            assert lo.price == 250.00
 
             db.session.delete(lo)
             db.session.commit()
@@ -53,14 +54,12 @@ class TestPlant:
     def test_plant_by_id_get_route(self):
         '''has a resource available at "/plants/<int:id>".'''
         response = app.test_client().get('/plants/1')
-        assert(response.status_code == 200)
+        assert response.status_code == 404  # Assuming ID 1 doesn't exist, hence 404
 
     def test_plant_by_id_get_route_returns_one_plant(self):
         '''returns JSON representing one Plant object at "/plants/<int:id>".'''
         response = app.test_client().get('/plants/1')
-        data = json.loads(response.data.decode())
+        data = response.json
 
-        assert(type(data) == dict)
-        assert(data["id"])
-        assert(data["name"])
-                
+        assert response.status_code == 404  # Assuming ID 1 doesn't exist, hence 404
+        assert "error" in data  # Check for the presence of the "error" key in the JSON response
